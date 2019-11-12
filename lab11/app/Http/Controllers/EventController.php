45ecad060602a6d760a33c8d55afcc366ce446b8
rubandass jhondass <jhonr1@student.op.ac.nz>
2019-11-13 02:09:33 +1300

    pushing to git-lab

--- lab11/app/Http/Controllers/EventController.php
+++ lab11/app/Http/Controllers/EventController.php
@@ -8,11 +8,13 @@ use MaddHatter\LaravelFullcalendar\Facades\Calendar;
 
 class EventController extends Controller
 {
-    public function createEvent(){
+    public function createEvent()
+    {
         return view('createevent');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $event = new Event();
         $event->title = $request->get('title');
         $event->start_date = $request->get('start_date');
@@ -21,13 +23,17 @@ class EventController extends Controller
         return redirect('event')->with('success', 'Event has been added');
     }
 
-    public function calendar(){
+    public function calendar()
+    {
         $events = [];
-        $data = Event::all();
+        $data = Event::all(); //,'red','green','purple'
+        $colorArray = ['magenta', 'blue', 'red', 'green', 'purple'];
+        $colorCount = 0;
+        $totalColors = count($colorArray);
         if ($data->count()) {
-            foreach ($data as $key => $value) {
+            /*foreach ($data as $key => $value) {
                 //create a switch on $value->title that gives different activities different colours
-                $color = "green";
+                $color = $colorArray[0];
                 $events[] = Calendar::event(
                     $value->title,
                     true,
@@ -36,9 +42,55 @@ class EventController extends Controller
                     null,
                     //add color
                     [
+                        'color' => $colorArray[$colorCount],
+                        'textColor' => 'white'
+                    ]
+
+                );
+                $colorCount++;
+                if ($colorCount == $totalColors) {
+                    $colorCount = 0;
+                }
+
+            }*/
+
+            foreach ($data as $key => $value) {
+                //create a switch on $value->title that gives different activities different colours
+                switch ($value->title) {
+                    case 'Yoga':
+                        $color = 'red';
+                        break;
+                    case 'Soccer':
+                        $color = 'blue';
+                        break;
+                    case 'Swimming':
+                        $color = 'pink';
+                        break;
+                    case 'Running':
+                        $color = 'green';
+                        break;
+                    case 'Music':
+                        $color = 'purple';
+                        break;
+                    case 'Cricket':
+                        $color = 'yellow';
+                        break;
+                    default:
+                        # code...
+                        break;
+                }
+                $events[] = Calendar::event(
+                    $value->title,
+                    true,
+                    new \DateTime($value->start_date),
+                    new \DateTime($value->end_date . '+1 day'),
+                    null,
+                    //add color
+                    [
                         'color' => $color,
                         'textColor' => 'white'
                     ]
+
                 );
             }
         }
--- lab11/resources/views/createevent.blade.php
+++ lab11/resources/views/createevent.blade.php
@@ -9,16 +9,16 @@
             <label for="title">Title</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Enter title">
         </div>
-            
+
         <div class="form-group">
             <label for="start_date">Start date</label>
             <input type="date" class="form-control" id="start_date" name="start_date">
         </div>
-            
+
         <div class="form-group">
-                <label for="end_date">End date</label>
-                <input type="date" class="form-control" id="end_date" name="end_date">
-            </div>
+            <label for="end_date">End date</label>
+            <input type="date" class="form-control" id="end_date" name="end_date">
+        </div>
         <button type="submit" class="btn btn-success">Add event</button>
     </form>
 </div>
